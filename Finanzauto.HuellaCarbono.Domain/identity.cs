501f4c972ee8758fdd6e0e70b704b8a643e98a69
julian david bojaca londoño <julyan9818@hotmail.com>
2023-02-07 22:01:40 -0500

    Ajuste clase identity

---
+++ Finanzauto.HuellaCarbono.Domain/identity.cs
@@ -0,0 +1,18 @@
+﻿using Finanzauto.HuellaCarbono.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finanzauto.HuellaCarbono.Domain
+{
+    public class identity : Ordering
+    {
+        public int idnId { get; set; }
+        public string idnDescription { get; set; }
+        public string idnImage { get; set; }
+        public double idnEquivalence { get; set; }
+        public int idnOrden { get; set; }
+    }
+}
